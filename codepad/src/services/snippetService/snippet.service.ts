516c1b560796b4bc4d4228f0950550d5305af06c
Patryk Dziarnecki <patryk.dziarnecki@gmail.com>
2019-10-06 16:00:26 +0200

    Adjusted snippet.service BASE_URL

--- codepad/src/services/snippetService/snippet.service.ts
+++ codepad/src/services/snippetService/snippet.service.ts
@@ -4,7 +4,8 @@ import * as https from "https";
 import {ISnippet, ISnippetService} from './types';
 
 // const BASE_URL = 'http://codepad.online'
-const BASE_URL = 'http://localhost:5000'
+// const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'http://codepad.online:5120'
 const SNIPPETS_API_URL = `${BASE_URL}/snippets`
 
 const instance = axios.create({
